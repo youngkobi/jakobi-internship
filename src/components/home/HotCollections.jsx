abae1b5b348a9216862dadec789e0cf2bdf6bedb
Jakobi <jahardy2021@gmail.com>
2025-08-31 18:32:38 -0500

    hotbar

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import Slider from "react-slick";
+import MultipleItems from "./slider";
+import { Carousel } from "react-bootstrap";
 
 const HotCollections = () => {
+  const [api, setApi] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
+  async function fetchApi() {
+    setLoading(true)
+    const { data } = await axios.get(
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
+    );
+    setApi(data);
+    setLoading(false)
+   
+  }
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,29 +33,30 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
-                </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <span>ERC-192</span>
-                </div>
-              </div>
-            </div>
-          ))}
+
+          {loading ? 
+            new Array(4).fill(0).map((element, index) => (
+              <div className="wrapper" key={index}>
+                          <div className="nft_coll  ">
+                            <div className="nft_wrap skelton-box">   
+                            </div>
+                            <div className="pp-skelly">
+                            <div className=" skelton-pp">
+                              _
+                            </div>
+                              <i className="fa fa-check"></i>
+                              </div>
+                            <div className="nft_coll_info ">
+                            
+                              <div className="skelton-title"></div>
+                              <span className="skelton-code"></span>
+                            </div>
+                          </div>
+                        </div>
+            ))
+           : (
+            <MultipleItems />
+          )}
         </div>
       </div>
     </section>
