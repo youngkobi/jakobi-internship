9250d3e98916a669090340416d3875634daa7c8e
Jakobi <jahardy2021@gmail.com>
2025-09-01 22:54:59 -0500

    top sellers componenet

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from "axios";
 
 const TopSellers = () => {
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
+  const [api, setApi] = useState([]);
+  const [loading, setLoading] = useState(true);
+  async function fetchApi() {
+    
+    const { data } = 
+    
+    await axios.get(
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
+    );
+    setApi(data);
+    setLoading(false)
+    console.log(data);
+  }
   return (
+
+    
     <section id="section-popular" className="pb-5">
       <div className="container">
         <div className="row">
@@ -15,21 +36,43 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
+             { loading
+             ?
+                new Array(12).fill(0).map((element, index) => (
+                  <li key={index}>
                   <div className="author_list_pp">
-                    <Link to="/author">
+                    
                       <img
                         className="lazy pp-author"
-                        src={AuthorImage}
+                        src={api.authorImage}
+                        alt=""
+                      />
+                      <div className=".pp-skelly"><div className="skelton-pp"></div></div>
+                      <i className="fa fa-check"></i>
+                  
+                  </div>
+                  <div className="author_list_info">
+                    <div className="skelton-top_title"></div>
+                    <div className="skelton-top_price"></div>
+                    
+                  </div>
+                </li>
+              ))
+             :  api.map((api, index) => (
+                <li key={index}>
+                  <div className="author_list_pp">
+                    <Link to={`/author/:${api.authorId}`}>
+                      <img
+                        className="lazy pp-author"
+                        src={api.authorImage}
                         alt=""
                       />
                       <i className="fa fa-check"></i>
                     </Link>
                   </div>
                   <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
+                    <Link to={`/author/:${api.authorId}`}>{api.authorName}</Link>
+                    <span>{api.price} ETH</span>
                   </div>
                 </li>
               ))}
